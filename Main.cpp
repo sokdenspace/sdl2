c3013a6f88d9bdb98fe99fb1b79fd02ace0ee0c2
Sok Den <sokdenspace@gmail.com>
2022-03-28 13:00:11 +0700

    update

--- Main.cpp
+++ Main.cpp
@@ -1,28 +1,23 @@
-// Example program:
-// Using SDL2 to create an application window
-
 #include "SDL.h"
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
 
-    SDL_Window* window;                    // Declare a pointer
-
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+    SDL_Window* window;
+    SDL_Init(SDL_INIT_VIDEO);
 
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
+        "2D Game",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        1280,
+        720,
+        SDL_WINDOW_OPENGL
     );
 
     // Check that the window was successfully created
     if (window == NULL) {
-        // In the case that the window could not be made...
         printf("Could not create window: %s\n", SDL_GetError());
         return 1;
     }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# sdl2
-I will create a 2d video game using SDL2.
+## Author: Sok Den										</br>
+## Date created: 03/28/2022								</br>
+## License: Sok Den										</br>
+--------------------------
+
+## `Configurations`										</br>
+## IDE: Visual Studio 2019								</br>
+
+### 1. Selcection										</br>
+Configuration: All Configurations						</br>
+Platform: All Platforms									</br>
+
+### 2.													</br>
+C/C++ -> Additional Include Directories:				</br>
+$(SolutionDir)Dependencies\SDL2\include					</br>
+
+Linker -> Additinal Library Directories:				</br>
+$(SolutionDir)Dependencies\SDL2\lib\x64					</br>
+
+
+I will create a 2d video game using SDL2.				</br>
